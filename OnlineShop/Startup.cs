04141e8b8801916fd5bbeb6d2970e80fb9c75dd9
faizahmed1314 <foyezahmedfj20@gmail.com>
2020-04-29 13:00:00 +0600

    Role controller and actions added

--- OnlineShop/Startup.cs
+++ OnlineShop/Startup.cs
@@ -37,7 +37,7 @@ namespace OnlineShop
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
